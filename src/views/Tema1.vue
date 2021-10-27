838bb4ee13662a58427b029decc333d83aa28237
Andrea Botello <andrea.botellor@misena.edu.co>
2021-10-27 15:29:37 -0500

    updated

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -33,7 +33,68 @@
 
     p.mb-4(data-aos="fade") De acuerdo con Robbins y Coulter (2010), hay <b>tres aspectos</b> en la definición de cultura que identifican y distinguen a una organización de otras, los cuales son:
 
-
+    .row.mb-4(data-aos="fade-down")
+      .col-lg-4.mb-3
+        .tarjeta-hover.bg-secundario.borde-curvo
+          .front-1
+            .row.justify-content-center
+              .col-md-6
+                h3.numero 01  
+              .col-md-4  
+                img(src='@/assets/curso/tarjea-ico1-a.svg', alt='Icono decorativo')
+              .col-md-10
+                h2.titulo Es una percepción
+                .rectangulo.mt-4
+          .front-2
+            .row.justify-content-center
+              .col-md-6
+                h3.numero 01
+              .col-md-4  
+                img(src='@/assets/curso/tarjea-ico1-b.svg', alt='Icono decorativo') 
+              .col-md-11
+                h4.mb-0.text-center Es una percepción
+                p.mb-0.text-center Cada persona percibe la organización conforme a las experiencias que ha vivido al interior de esta.    
+      .col-lg-4.mb-3.h-100
+        .tarjeta-hover.bg-secundario.borde-curvo(@mouseover="mostrarIndicador = false")
+          .indicador--click(v-if="mostrarIndicador")
+          .front-1
+            .row.justify-content-center
+              .col-md-6
+                h3.numero 02
+              .col-md-4  
+                img(src='@/assets/curso/tarjea-ico2-a.svg', alt='Icono decorativo')
+              .col-md-10
+                h2.titulo Es descriptiva
+                .rectangulo.mt-4
+          .front-2
+            .row.justify-content-center
+              .col-md-6
+                h3.numero 02
+              .col-md-4  
+                img(src='@/assets/curso/tarjea-ico2-b.svg', alt='Icono decorativo') 
+              .col-md-11
+                h4.mb-0.text-center Es descriptiva
+                p.mb-0.text-center Percepción conjunta de los empleados de la organización, independiente de si es positiva o negativa.    
+      .col-lg-4.mb-3.h-100
+        .tarjeta-hover.bg-secundario.borde-curvo
+          .front-1
+            .row.justify-content-center
+              .col-md-6
+                h3.numero 03
+              .col-md-4  
+                img(src='@/assets/curso/tarjea-ico3-a.svg', alt='Icono decorativo')
+              .col-md-10
+                h2.titulo Es compartida
+                .rectangulo.mt-4    
+          .front-2
+            .row.justify-content-center
+              .col-md-6
+                h3.numero 03
+              .col-md-4  
+                img(src='@/assets/curso/tarjea-ico3-b.svg', alt='Icono decorativo') 
+              .col-md-11
+                h4.mb-0.text-center Es compartida
+                p.mb-0.text-center El conjunto de empleados describen la cultura de su organización de manera análoga sin tomar en cuenta el cargo o las experiencias vividas.    
 
 
     p.mb-4(data-aos="fade") Así las cosas, se define la cultura organizacional de la siguiente manera:
@@ -75,7 +136,7 @@
         figure
           img(src='@/assets/curso/tema1-fig1.svg', alt='Cultura organizacional')
       .col-md-7.mb-4  
-        .row.justify-content-center.bordes-curvos.bg-secundario.py-4
+        .row.justify-content-center.bordes-curvos.bg-shadow.bg-secundario.py-4
           .col-md-2.col-4.mb-3.mb-md-0 
             img(src='@/assets/curso/tema1-ico1.svg', alt='Cultura organizacional')
           .col-md-8
@@ -215,7 +276,7 @@
 
     .row.justify-content-center
       .col-md-8
-        .row.justify-content-center.bordes-curvos.bg-secundario.py-4
+        .row.justify-content-center.bordes-curvos.bg-secundario.bg-shadow.py-4
           .col-md-2.col-4.mb-3.mb-md-0 
             img.px-md-2.p-0(src='@/assets/curso/tema1-ico1.svg', alt='Cultura organizacional')
           .col-md-8
@@ -231,19 +292,166 @@
 
     p.mb-4(data-aos="fade") La cultura organizacional brinda diferentes formas determinadas de pensamiento, emociones y reacciones que conllevan a una adecuada toma de decisiones. Existen dos clases de elementos: 
 
-    .row.justify-content-center.bg-cuadro-verde.mb-4(data-aos="fade-down") 
-      .col-md-10
+    .row.justify-content-center.mb-4(data-aos="fade-down") 
+      .col-md-10.pb-5.bg-cuadro-verde
         .row.justify-content-center.tarjetas-iconos
-          .col-md-5.bg-azul3.borde-curvo.mx-2
+          .col-md-5.bg-azul3.borde-curvo.bg-tarjeta1.bg-shadow.mx-2.p-4
             img.mb-4(src='@/assets/curso/tema1-1-ico1.svg', alt='Icono decorativo')
             p.text-center Los ocultos, que no se observan con claridad y que se pueden inferir a partir de aquellos que son visibles.
-          .col-md-5.bg-azul4.borde-curvo.mx-2
+          .col-md-5.bg-azul4.borde-curvo.bg-tarjeta2.bg-shadow.mx-2.p-4
             img.mb-4(src='@/assets/curso/tema1-1-ico2.svg', alt='Icono decorativo')
             p.text-center Los observables, son todos aquellos que se pueden analizar, contemplar, palpar. 
     
     p.mb-4(data-aos="fade") Se invita a analizar la siguiente tabla, donde se presentan los elementos de la cultura organizacional. (Hellriegel et al., 2017, p. 596-600):
 
+    .titulo-sexto.color-acento-contenido
+      p.mb-0 <b>Tabla 2.</b> Elementos de la cultura organizacional
+    .tabla-a.color-acento-contenido 
+      table.tabla-2
+        caption Nota. Adaptado de Llanos, 2018.
+        thead
+          tr
+            th Clase
+            th Elementos
+            th Definición
+        tbody
+          tr
+            td.text-center(rowspan="3") 
+              strong OCULTOS
+            td.text-center Premisas
+            td Ideas y sentimientos que se consideran ciertos y se dan por sentado.
+          tr
+            td.text-center Valores
+            td Creencias básicas e importantes, tienen sentido y son estables en el tiempo.
+          tr
+            td.text-center Normas
+            td Reglas a seguir por los diferentes equipos de la organización.   
+          tr
+            td.text-center(rowspan="5") 
+              strong OBSERVABLES
+            td.text-center Socialización
+            td Proceso establecido para adaptar a los nuevos integrantes a la cultura de la entidad.
+          tr
+            td.text-center Símbolos
+            td Objeto visible que se utiliza para visibilizar un valor abstracto o significado especial.
+          tr
+            td.text-center Lenguaje
+            td Sistema utilizado para transmitir significados especiales: sonidos vocales, signos escritos y gestos compartidos.
+          tr
+            td.text-center Relatos
+            td Todo aquello que muestra los logros obtenidos por los líderes de la organización a lo largo del tiempo: leyendas, mitos, hazañas y casos.
+          tr
+            td.text-center Prácticas
+            td 
+              ul.lista-ul--color.lista-naranja
+                li
+                  i.fas.fa-dot-circle
+                  p.mb-0
+                    strong Tabúes: 
+                    | Toda conducta que prohíbe por la cultura.     
+                li
+                  i.fas.fa-dot-circle
+                  p.mb-0
+                    strong Ceremonias: 
+                    | Actividades formales que generan sentimientos fuertes.
 
+    Separador
+
+
+    // 1.2
+    #t_1_2.titulo-segundo(data-aos="fade-right")
+      .h2 1.2  Técnicas de segmentación
+
+    .row
+      .col-md-7.bloque-fondo-figura.mb-4(data-aos="fade-right")
+        p Es un reto atraer el mejor talento y retenerlo, por lo tanto, es 
+          strong.etiqueta importante buscar la satisfacción y el bienestar de los empleados
+          | que, al estar alineados con el sentir de la organización, darán como resultado mayor productividad y la prestación de un servicio de calidad. Desde este punto de vista, es fundamental que se cuente con una propuesta de valor llamativa para el cliente interno que contemple su ciclo de vida en la empresa en especial en las etapas de desarrollo y retención.
+      .col-md-5.mb-4(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema1-2-img.png', alt='Texto que describa la imagen') 
+
+    p.mb-4(data-aos="fade") Para poder llegar a una propuesta de valor, primero se deben tomar en cuenta las características de la cultura organizacional que son primordiales al momento de medir el rendimiento individual y colectivo del talento humano en una organización. Estas características son, ver figura 3:
+
+    .row.justify-content-center.mb-4(data-aos="fade-down") 
+      .col-md-10
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 <b>Figura 3.</b> Características de la cultura organizacional
+        .row.justify-content-center
+          .col-md-8
+            figure
+              img.mb-2(src='@/assets/curso/tema1-2-figura3.svg', alt='Tipos de comunicación no verbal')
+            figcaption Nota. Características de la cultura organizacional. Adaptado de Robbins y Judge (2009).
+
+    p(data-aos="fade") Ya con un contexto de la organización, se hace más claro realizar una segmentación acertada para que los empleados respondan como se espera que lo hagan. 
+
+    p(data-aos="fade") Para afianzar los conocimientos en establecer las características de la cultura organizacional en la empresa, se  puede revisar en el material complementario, el video 
+      strong.etiqueta ¿Qué es cultura organizacional y còmo se crea? 
+
+    p.mb-4(data-aos="fade") Entre las técnicas de segmentación se encuentra:
+
+    .fig-verde.p-4
+      h3 El análisis RFM
+
+    .row
+      .col-md-6.mb-4(data-aos="fade-right")
+        p Es una técnica de marketing que sirve para determinar cuáles son los mejores clientes o a nivel interno, empleados. Las variables utilizadas para hacer la medición son:
+        ul.lista-ul--color.lista-naranja
+          li.mb-1
+            i.fas.fa-dot-circle
+            p.mb-0
+              strong R: Recency = 
+              | recencia
+          li.mb-1
+            i.fas.fa-dot-circle
+            p.mb-0
+              strong F: Frequency = 
+              | frecuencia
+          li.mb-1
+            i.fas.fa-dot-circle
+            p.mb-0
+              strong M: Monetary =  
+              | monetización        
+      .col-md-6.mb-4.bg-forma5.bg-verde.bordes-curvos.bg-shadow.py-4.px-5.figuras(data-aos="fade-left")
+        img.fig-2(src='@/assets/curso/forma-4.png', alt='Forma decorativa')  
+        .row.justify-content-center
+          .col-md-11.px-md-2
+            p.mb-0 Esta técnica funciona como una instantánea del comportamiento que tienen los clientes internos a la fecha, permite tener una visión de la dinámica organizacional, tomar mejores decisiones desde la perspectiva actual y crear ventajas competitivas conforme a las tendencias del mercado.
+        img.fig-3(src='@/assets/curso/forma-4.png', alt='Forma decorativa')
+
+    p(data-aos="fade") Es necesario realizar el análisis RFM en diferentes etapas y usar los resultados para lograr un histórico que permita ver la tendencia de cada empleado.
+
+    p(data-aos="fade") Con la información obtenida, se segmentan a los empleados en categorías afines que sirven para definir qué acciones de fidelización se han de tomar y que sean adaptables a cada grupo. 
+
+    p.mb-4(data-aos="fade") Los beneficios que trae el análisis son:
+
+    .row.justify-content-center.tarjetas-iconos.mb-4(data-aos="fade-down") 
+      .col-md-3.bg-azul3.borde-curvo.bg-tarjeta1.p-4
+        img.mb-4(src='@/assets/curso/tema1-2-tarj1.svg', alt='Icono decorativo')
+        h4.text-center Una mayor retención de empleados.
+      .col-md-3.bg-azul4.bg-tarjeta2.p-4
+        img.mb-4(src='@/assets/curso/tema1-2-tarj2.svg', alt='Icono decorativo')
+        h4.text-center Incremento en la tasa de respuesta. 
+      .col-md-3.bg-azul3.bg-tarjeta1.p-4
+        img.mb-4(src='@/assets/curso/tema1-2-tarj3.svg', alt='Icono decorativo')
+        h4.text-center Aumento en la tasa de conversión.
+      .col-md-3.bg-azul4.borde-curvo-abajo.bg-tarjeta2.p-4
+        img.mb-4(src='@/assets/curso/tema1-2-tarj4.svg', alt='Icono decorativo')
+        h4.text-center Incremento de los ingresos.
+
+    p.mb-4(data-aos="fade") En la siguiente figura 4 se visualiza un ejemplo del análisis RFM aplicado al área de ventas para los asesores comerciales, con el propósito de aplicar un nuevo plan de compensaciones económicas, de acuerdo con el cumplimiento de metas y establecer un plan de capacitaciones personalizadas.
+    
+    .row.justify-content-center(data-aos="fade-down") 
+      .col-md-10
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 <b>Figura 4.</b> Análisis RFM
+      .col-md-10
+        .row.justify-content-center.fondo-figura4.p-md-5.p-3
+          h4.text-center MODELO RFM
+          .col-md-10
+            figure
+              img(src='@/assets/curso/tema1-2-figu4.svg', alt='MODELO RFM')
+            figcaption Nota. Factores a tener en cuenta en el análisis RFM para asesores comerciales. Adaptado de Valencia y Cabañas (2017).
 
 
   
@@ -255,6 +463,7 @@ export default {
   components: {},
   data: () => ({
     // variables de vue
+    mostrarIndicador: true,
   }),
   mounted() {
     this.$nextTick(() => {
